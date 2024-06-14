db27f6ba464d36fff368f345009638d0c1f3a834
shobha <shobhakushwaha1203@gmail.com>
2024-06-14 06:04:28 -0700

    initial commit

---
+++ src/app/services/Master/bank.service.ts
@@ -0,0 +1,58 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UrlService } from '../URL/url.service';
+import { AuthService } from '../Auth-Service/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+  export class BankService{
+
+    constructor(private _http: HttpClient, private _urlService: UrlService, private authService: AuthService) { }
+  
+    private headers = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8',
+      // 'Authorization': this.authservice.getAccessTokenNew()
+    });
+  
+  
+    getPaymentList(code: any) {
+      let url = this._urlService.API_ENDPOINT_BANK_MASTER + `/${code}`
+      return this._http.get(url, { headers: this.headers });
+    }
+  
+  
+    savePayment(payload: any): Observable<any> {
+      // const data = {
+      //   paymentListUpList: [payload]
+      // };
+  
+  
+      let url = `${this._urlService.API_ENDPOINT_BANK_MASTER}/GetBankMasterList`;
+      return this._http.post(url, payload, { headers: this.headers });
+    }
+  
+  
+    
+      updatePayMent(payload: any): Observable<any> {
+        // const data = {
+        //   paymentListUpList: [payload]
+        // };
+    
+    
+        let url = `${this._urlService.API_ENDPOINT_BANK_MASTER}/SaveBankMaster`;
+        return this._http.post(url, payload, { headers: this.headers });
+      }
+    
+    deletePaymentList(code: number, reason: any) {
+     let url = this._urlService.API_ENDPOINT_BANK_MASTER + "/DeleteBankMaster" + `?code=${code}&UserMaster_Code=13&ReasonForDelete=${reason}`;
+      return this._http.post(url, '', { headers: this.headers });
+    }
+  
+  
+  
+   
+  }
+
