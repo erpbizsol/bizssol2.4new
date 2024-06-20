5f7e2fa192ce728b639930b7695b75f75076ad4b
shobha <shobhakushwaha1203@gmail.com>
2024-06-20 03:27:52 -0700

    complted bank master

---
+++ src/app/BizsolErp/Masters/godown/godown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-godown',
+  standalone: true,
+  imports: [],
+  templateUrl: './godown.component.html',
+  styleUrl: './godown.component.scss'
+})
+export class GodownComponent {
+
+}
